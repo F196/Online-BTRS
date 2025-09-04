bf2bcfef27c5bbc169cbbc15e3852bd50857e1ce
Collins <75946061+F196@users.noreply.github.com>
2025-09-04 01:36:58 -0700

    Update add_schedule.php

--- BTRS-ONLINE/BTRS/colo/backend/add_schedule.php
+++ BTRS-ONLINE/BTRS/colo/backend/add_schedule.php
@@ -5,13 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $departure_location = $_POST['departure'];
     $arrival_location = $_POST['arrival'];
 
-    // Combine date and time for departure and arrival
+    /
     $departure_datetime = $_POST['departure_date'] . ' ' . $_POST['departure_time'];
     $arrival_datetime = $_POST['arrival_date'] . ' ' . $_POST['arrival_time'];
 
     $bus_id = $_POST['bus_id'];
 
-    // Insert into the database
+    
     $stmt = $conn->prepare("INSERT INTO Routes (DepartureLocation, ArrivalLocation, DepartureTime, ArrivalTime, BusID) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssi", $departure_location, $arrival_location, $departure_datetime, $arrival_datetime, $bus_id);
 
@@ -32,4 +32,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <a href="bus_schedule_management.php"><button>Back to Scheduler</button></a>
 </body>
+
 </html>
